87bd857d51fb5573c8f5b2a3c1f77d00f1522875
imoxto <mohammad.rafid.hamid@g.bracu.ac.bd>
2023-11-28 16:22:57 +0600

    sheet write

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import * as XLSX from "xlsx";
 
 export function Messager() {
   const [file, setFile] = useState<File | null>(null);
+  const [data, setData] = useState<string[][] | null>(null);
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files ? e.target.files[0] : null;
@@ -25,17 +26,36 @@ export function Messager() {
         const workbook = XLSX.read(binaryStr, { type: "binary" });
         const sheetName = workbook.SheetNames[0];
         const worksheet = workbook.Sheets[sheetName];
-        const data: any[] = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-        console.log(data);
+        const data: any[][] = XLSX.utils.sheet_to_json(worksheet, {
+          header: 1,
+        });
+        setData(data);
       }
     };
     reader.readAsBinaryString(file);
   };
+
+  const handleFileDownload = async () => {
+    if (!data) {
+      console.log("No data");
+      return;
+    }
+    const modifiedData = data.map((row) => [...row, "Hello World"]); // Add new column
+
+    const newWorksheet = XLSX.utils.aoa_to_sheet(modifiedData);
+    const newWorkbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(newWorkbook, newWorksheet, "Modified Sheet");
+
+    XLSX.writeFile(newWorkbook, "modified_file.xlsx");
+  };
   return (
     <div className="flex flex-col items-center gap-4 justify-center">
       <input type="file" onChange={handleFileChange} />
-      <Button variant="destructive" onClick={handleFileRead}>
+      <Button variant="secondary" onClick={handleFileRead}>
         Read File
+      </Button>
+      <Button variant="outline" onClick={handleFileDownload}>
+        Download New File
       </Button>
       <Button
         onClick={() => {
